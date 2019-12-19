1b65baf229f1a7163dc70f6350410ea8592746dc
Michał Kowalkowski <kowalkowski.michal@gmail.com>
2019-12-19 18:10:46 +0100

    Update RCTBluetoothSerial.h

--- ios/RCTBluetoothSerial/RCTBluetoothSerial.h
+++ ios/RCTBluetoothSerial/RCTBluetoothSerial.h
@@ -11,8 +11,16 @@
  
  */
 
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
+#if __has_include(<React/RCTEventEmitter.h>)
 #import <React/RCTEventEmitter.h>
+#else
+#import "RCTEventEmitter.h"
+#endif
 #import "BLE.h"
 
 typedef void (^RCTBluetoothSerialPeripheralCallback) (CBPeripheral *_Nullable peripheral);
